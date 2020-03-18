01f31c95ebdb9502d73127dc5f582f9749276e17
shupei <freddiechang1994@live.com>
2020-03-17 21:08:23 -0600

    lab4 ex11, maybe still sth wrong with env

--- kern/trap.c
+++ kern/trap.c
@@ -395,11 +395,45 @@ page_fault_handler(struct Trapframe *tf)
 	//   (the 'tf' variable points at 'curenv->env_tf').
 
 	// LAB 4: Your code here.
-
-	// Destroy the environment that caused the fault.
-	cprintf("[%08x] user fault va %08x ip %08x\n",
+	if(curenv->env_pgfault_upcall == NULL)
+	{
+		cprintf("[%08x] user fault va %08x ip %08x\n",
 		curenv->env_id, fault_va, tf->tf_eip);
-	print_trapframe(tf);
-	env_destroy(curenv);
+		cprintf("No page fault handler found.\n");
+	    print_trapframe(tf);
+	    env_destroy(curenv);
+	}
+	else
+	{
+		user_mem_assert(curenv, (void *)(UXSTACKTOP - PGSIZE), PGSIZE, PTE_P|PTE_W);
+		uint32_t * stacktop = (uint32_t *)UXSTACKTOP;
+		if(tf->tf_esp >= UXSTACKTOP-PGSIZE && tf->tf_esp <= UXSTACKTOP - 1)
+		{
+			stacktop = (uint32_t *)tf->tf_esp;
+			stacktop --;
+			*stacktop = 0;
+		}
+		struct UTrapframe *utf = ((void *)stacktop - sizeof(struct UTrapframe));
+		if(utf < (struct UTrapframe *)(UXSTACKTOP-PGSIZE))
+		{
+			cprintf("Run out of space on UXSTACK.\n");
+			print_trapframe(tf);
+	        env_destroy(curenv);
+		}
+
+		utf->utf_esp = tf->tf_esp;
+		utf->utf_eflags = tf->tf_eflags;
+		utf->utf_eip = tf->tf_eip;
+		utf->utf_regs = tf->tf_regs;
+		utf->utf_err = tf->tf_err;
+		utf->utf_fault_va = tf->tf_esp;
+
+		//tf->tf_esp -= 4;
+		//*(int32_t *)(tf->tf_esp) = tf->tf_eip;
+		tf->tf_eip = (uint32_t)curenv->env_pgfault_upcall;
+		env_run(curenv);
+	}
+	
+	// Destroy the environment that caused the fault.
 }
 
